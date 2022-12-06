8a7bf1c357ec8b3e6f202833dc3b4f7548143b92
Jenith Mercer <jamercer2012@gmail.com>
2022-12-06 15:30:58 -0600

    new dot squishes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, MouseEvent, useState } from 'react';
+import React, { CSSProperties, MouseEvent, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,9 +9,11 @@ interface point {
 }
 
 function App() {
+  const [pointerPos, setPointerPos] = useState<point>({x:0, y:0});
   const [newDot, setNewDot] = useState<point|undefined>();
   const [dotList, setDotList] = useState<point[]>([]);
   const [undoDots, setUndoDots] = useState<point[]>([]);
+
 
   const mouseDownAction = (e:MouseEvent) => {
     e.preventDefault()
@@ -26,7 +28,11 @@ function App() {
       setUndoDots([]);
     }
   }
-  
+
+  const mouseMoveAction = (e:MouseEvent) => {
+    setPointerPos({x:e.pageX, y:e.pageY});
+  }
+
   const stopProp = (e:MouseEvent) => {
     e.stopPropagation();
   }
@@ -60,13 +66,32 @@ function App() {
     });
   }
 
+  const renderNewDot = (dotStart: point|undefined, mousePos: point) => {
+    if (dotStart === undefined) return;
+
+    const diff: point = {x: dotStart.x-mousePos.x, y: dotStart.y-mousePos.y};
+    const magnitude: number = Math.sqrt(Math.pow(diff.x, 2) + Math.pow(diff.y, 2));
+    const scale: number = Math.max(1, magnitude / 32);
+    const rot: number = Math.atan2(diff.y, diff.x);
+    const transformStyle: string = 'rotate('+rot+'rad) translateX(-'+magnitude/2+'px) scaleX('+scale+')'
+    
+    const style: CSSProperties = {
+      position: 'absolute',
+      left: dotStart.x - 16,
+      top: dotStart.y - 16,
+      transform: transformStyle,
+    }
+    return <div className='newDot' style={style}></div>
+  }
+
   return (<div>
-    <div className="App" onMouseDown={mouseDownAction} onMouseUp={mouseUpAction}>
+    <div className="App" onMouseDown={mouseDownAction} onMouseUp={mouseUpAction} onMouseMove={mouseMoveAction}>
       <div className='button-well'>
         <button onMouseDown={stopProp} onMouseUp={mouseUpAction} onClick={undo}>undo</button>
         <button onMouseDown={stopProp} onMouseUp={mouseUpAction} onClick={redo}>redo</button>
       </div>
       {renderDots(dotList)}
+      {renderNewDot(newDot, pointerPos)}
     </div>
   </div>);
 }
